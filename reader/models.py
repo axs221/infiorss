cc1784d021e1c6440fcb94f90acc425aceecf691
Shawn Axsom <axs221@gmail.com>
2015-01-02 09:51:34 -0500

    Allow blank titles, to prevent error 'This field is required'

--- reader/models.py
+++ reader/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Feed(models.Model):
     uri = models.CharField(max_length=300)
-    title = models.CharField(max_length=300, default="News")
+    title = models.CharField(max_length=300, default="News", null=True, blank=True)
 
     def __str__(self):
         return self.title
